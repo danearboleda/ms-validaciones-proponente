1871ebc19f3b959f2d9ae04a88d72c57c7fba3fd
cocas <miguel.1701716489@ucaldas.edu.co>
2021-12-02 09:47:54 -0500

    Correccion de relacion proponente departamento

--- src/repositories/proponente.repository.ts
+++ src/repositories/proponente.repository.ts
@@ -1,8 +1,9 @@
 import {inject, Getter} from '@loopback/core';
 import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
 import {MysqlDataSource} from '../datasources';
-import {Proponente, ProponenteRelations, TipoVinculacion} from '../models';
+import {Proponente, ProponenteRelations, TipoVinculacion, Departamento} from '../models';
 import {TipoVinculacionRepository} from './tipo-vinculacion.repository';
+import {DepartamentoRepository} from './departamento.repository';
 
 export class ProponenteRepository extends DefaultCrudRepository<
   Proponente,
@@ -12,10 +13,14 @@ export class ProponenteRepository extends DefaultCrudRepository<
 
   public readonly vinculaciones: BelongsToAccessor<TipoVinculacion, typeof Proponente.prototype.id>;
 
+  public readonly tiene_departamento: BelongsToAccessor<Departamento, typeof Proponente.prototype.id>;
+
   constructor(
-    @inject('datasources.mysql') dataSource: MysqlDataSource, @repository.getter('TipoVinculacionRepository') protected tipoVinculacionRepositoryGetter: Getter<TipoVinculacionRepository>,
+    @inject('datasources.mysql') dataSource: MysqlDataSource, @repository.getter('TipoVinculacionRepository') protected tipoVinculacionRepositoryGetter: Getter<TipoVinculacionRepository>, @repository.getter('DepartamentoRepository') protected departamentoRepositoryGetter: Getter<DepartamentoRepository>,
   ) {
     super(Proponente, dataSource);
+    this.tiene_departamento = this.createBelongsToAccessorFor('tiene_departamento', departamentoRepositoryGetter,);
+    this.registerInclusionResolver('tiene_departamento', this.tiene_departamento.inclusionResolver);
     this.vinculaciones = this.createBelongsToAccessorFor('vinculaciones', tipoVinculacionRepositoryGetter,);
     this.registerInclusionResolver('vinculaciones', this.vinculaciones.inclusionResolver);
   }
