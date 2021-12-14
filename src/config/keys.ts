10d873cfc6149cea24b14e54eb1b369f23edb56a
Brayan Stiven Cifuentes <1704stiven@gmail.com>
2021-12-13 22:12:09 -0500

    Creacion de la estrategia para el  authenticate del admin

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -6,4 +6,9 @@ export namespace Keys {
   export const carpetaDocumentoPersona = '../../archivos/documentos';
   export const nombreCampoDocumentoPersona = 'file';
   export const extensionesPermitidasDOC: string[] = ['.PDF', '.DOC', '.DOCX', '.XLS', '.XLSX'];
+  export const rol_administrador = "6182f9183c21c2334c6b35a9";
+  export const rol_supervisor_solicitudes = "619e770494e5d416a8b6514b";
+  export const url_validar_token = "http://localhost:5001/validar-token";
+  export const arg_token = "token";
+  export const arg_rol_token = "rol";
 }
